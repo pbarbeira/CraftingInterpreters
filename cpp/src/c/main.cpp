1a9491be1d123818fc5dd1d986fcaf0db686bd27
pedrobarbeira <pedro.barbeira95@gmail.com>
2025-04-07 21:29:28 +0100

    First commit

---
+++ cpp/src/c/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by pbarbeira on 07-04-2025.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World Clox!" << std::endl;
+    return 0;
+}
---
+++ cpp/tools/java/GenerateAst.cpp
@@ -0,0 +1,9 @@
+//
+// Created by pbarbeira on 07-04-2025.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Generating AST...\n";
+}
